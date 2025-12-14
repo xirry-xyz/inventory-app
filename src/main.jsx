8853973b59795bc93fc9c01e622a2ebbd4f6ee69
Xirry <xirry@XirrydeMacBook-Pro.local>
2025-12-15 00:16:34 +0800

    feat: implement theme provider context

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // 确保从正确的路径导入主组件
-import App from './App.jsx'; 
+import App from './App.jsx';
 // 确保导入全局样式文件
-import './index.css'; 
+import './index.css';
+import { ThemeProvider } from "./components/ThemeProvider";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App /> 
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
